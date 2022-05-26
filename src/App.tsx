73181536ac67e31258a1f6024509565837306ede
Muhammad Raihan Satrio <adewipo@gmail.com>
2022-05-26 23:16:01 +0700

    feat: added prototype navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,10 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.scss'
+import { Route, Routes } from "react-router-dom";
+import { Beranda } from "./pages/Beranda";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" className="btn btn-primary" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
-
-export default App
+export const App = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Beranda/>} />
+        </Routes>
+    );
+};
